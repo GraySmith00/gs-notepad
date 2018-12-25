0b44e6580fa64d120fd0e3ec0a391e1d52a8ed9e
Gray Smith <gray.smith00@gmail.com>
2018-12-24 22:25:16 -0700

    Fix JSON response on create to show item

--- notepad-api/notes/create.js
+++ notepad-api/notes/create.js
@@ -33,7 +33,7 @@ module.exports.create = (event, context, callback) => {
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item)
+      body: JSON.stringify(params.Item)
     };
     callback(null, response);
   });
