8efcedc39c81963b3ce2a8121931738ae38d493c
Gray Smith <gray.smith00@gmail.com>
2018-12-26 22:29:07 -0700

    Call getNotes on App componentDidMount

--- notepad-client/src/components/App/App.js
+++ notepad-client/src/components/App/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import { postNote } from '../../lib/apiCalls';
+
+import { postNote, getNotes } from '../../lib/apiCalls';
 
 import NoteForm from '../NoteForm';
+import NotesList from '../NotesList';
 
 class App extends Component {
   state = {
     notes: []
   };
+
+  async componentDidMount() {
+    const notes = await getNotes();
+    this.setState({ notes });
+  }
 
   addNote = async note => {
     const savedNote = await postNote(note);
@@ -15,10 +22,13 @@ class App extends Component {
   };
 
   render() {
+    const { notes } = this.state;
+
     return (
       <div className="App">
         <h1>Hello</h1>
         <NoteForm addNote={this.addNote} />
+        <NotesList notes={notes} />
       </div>
     );
   }
