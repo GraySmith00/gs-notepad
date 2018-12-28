17ce793e647503539db402d3086cd963086a6fd2
Gray Smith <gray.smith00@gmail.com>
2018-12-27 23:37:28 -0700

    NotesList test 100% coverage

--- notepad-client/src/components/NotesList/NotesList.test.js
+++ notepad-client/src/components/NotesList/NotesList.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { mockNotes } from '../../lib/mockData';
 import NotesList from './NotesList';
+import Note from '../Note';
 
 describe('NotesList component', () => {
   let wrapper;
@@ -14,5 +15,21 @@ describe('NotesList component', () => {
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should filter the notes if there is a valid tagFilter', () => {
+    wrapper = shallow(
+      <NotesList notes={mockNotes} tagFilter="work" dateFilter={null} />
+    );
+
+    expect(wrapper.children().find(Note).length).toEqual(2);
+  });
+
+  it('should filter the notes if there is a valid dateFilter', () => {
+    wrapper = shallow(
+      <NotesList notes={mockNotes} tagFilter={null} dateFilter={'12/21/2018'} />
+    );
+
+    expect(wrapper.children().find(Note).length).toEqual(1);
   });
 });
