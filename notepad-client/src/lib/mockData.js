7b4fa231badc1ccc73bcd5fd0d69bb70d1f210a3
Gray Smith <gray.smith00@gmail.com>
2018-12-27 18:04:44 -0700

    Add mockNotes to mockData file

---
+++ notepad-client/src/lib/mockData.js
@@ -0,0 +1,38 @@
+export const mockNotes = [
+  {
+    createdAt: 1545951483012,
+    text: 'buy groceries',
+    tag: 'hobby',
+    id: 'dd5aa440-0a2a-11e9-93fa-a7949507e226'
+  },
+  {
+    createdAt: 1545951433152,
+    text: 'do laundry',
+    tag: 'work',
+    id: 'bfa29c00-0a2a-11e9-93fa-a7949507e226'
+  },
+  {
+    createdAt: 1545951460092,
+    text: 'play golf',
+    tag: 'hobby',
+    id: 'cfb153c0-0a2a-11e9-93fa-a7949507e226'
+  },
+  {
+    createdAt: 1545951504338,
+    text: 'buy gifts',
+    tag: 'personal',
+    id: 'ea10bb20-0a2a-11e9-93fa-a7949507e226'
+  },
+  {
+    createdAt: 1545451632475,
+    text: 'set up tv',
+    tag: 'personal',
+    id: '425ca4b0-0a2b-11e9-93fa-a7949507e226'
+  },
+  {
+    createdAt: 1545951440518,
+    text: 'run errands',
+    tag: 'work',
+    id: 'c4069260-0a2a-11e9-93fa-a7949507e226'
+  }
+];
