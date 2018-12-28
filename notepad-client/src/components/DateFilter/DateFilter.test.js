77c56d647fc85b2e77743883b79c767000d041f6
Gray Smith <gray.smith00@gmail.com>
2018-12-27 23:36:59 -0700

    DateFilter test 100% coverage

--- notepad-client/src/components/DateFilter/DateFilter.test.js
+++ notepad-client/src/components/DateFilter/DateFilter.test.js
@@ -17,4 +17,16 @@ describe('DateFilter component', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should call setDateFilter on change', () => {
+    expect(wrapper.find('.date-filter select').length).toEqual(1);
+
+    wrapper.find('.date-filter select').simulate('change', {
+      target: {
+        value: '12/21/2018'
+      }
+    });
+
+    expect(mockSetDateFilter).toHaveBeenCalled();
+  });
 });
